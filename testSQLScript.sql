ce0e323aa864931bbf81a61fcd25563eaf302c6c
minhvu16 <minhvu16@pender.students.cs.ubc.ca>
2024-10-19 15:20:33 -0700

    modfied testScript

--- testSQLScript.sql
+++ testSQLScript.sql
@@ -1,6 +1,14 @@
 create table Movie {
     MovieID integer primary key,
     Title char(50),
-    Year integer
+    Year integer 
 };
 
+create table Author {
+    AuthorID char(30) primary key,
+    AuthorName char(50),
+    MovieID integer REFERENCES MovieID.Movie ON DELETE CASCADE
+    BirthYear integer 
+};
+
+create
