f3ec39279e7f5300fcb640631bcac9019cd77505
Khue Do <khuedo22@students.cs.ubc.ca>
2024-11-24 20:27:48 -0800

    fixed sql parsing

--- appService.js
+++ appService.js
@@ -199,15 +199,15 @@ async function countDemotable() {
 
 //SELECTION
 async function selectStops(selectedAttributes, condition) {
-    const sqlQuery = `SELECT ${selectedAttributes} FROM Stops WHERE {condition}`;
-//    return await withOracleDB(async (connection) => {
-//        const result = await connection.execute(sqlQuery);
-//
-//        return result;
-//    }).catch(() => {
-//        return false;
-//    });
-    return sqlQuery;
+    const sqlQuery = `SELECT ${selectedAttributes} FROM Stops WHERE ${condition}`;
+    return await withOracleDB(async (connection) => {
+        const result = await connection.execute(sqlQuery);
+
+        return result;
+    }).catch(() => {
+        return false;
+    });
+//    return sqlQuery;
 }
 
 //PROJECTION
