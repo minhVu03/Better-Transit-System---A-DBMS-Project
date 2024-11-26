edf4823d99b3d424138d7b0e4dad0fc2eca76fe1
Khue Do <khuedo22@students.cs.ubc.ca>
2024-11-25 23:33:42 -0800

    fixed sql query

--- appService.js
+++ appService.js
@@ -217,10 +217,7 @@ async function joinTripsplan2People(name, transitCard) {
     const sqlQuery = `SELECT tp.startTime, tp.arrivalLocation, tp.departureLocation FROM TripsPlan2 tp, People
              WHERE p.customerID = tp.customerID AND p.peopleName=${name} AND p.transitCardNumber=${transitCard}`
     return await withOracleDB(async (connection) => {
-        const result = await connection.execute(
-            `SELECT tp.startTime, tp.arrivalLocation, tp.departureLocation FROM TripsPlan2 tp, People
-             WHERE p.customerID = tp.customerID AND p.peopleName=:name AND p.transitCardNumber=:transitCard`
-             [name, transitCard]);
+        const result = await connection.execute(sqlQuery);
 
         return result;
     }).catch(() => {
