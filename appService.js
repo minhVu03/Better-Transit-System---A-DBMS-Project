f44ccc59ff352d2b5798061db6e48fdf097dd594
Khue Do <khuedo22@students.cs.ubc.ca>
2024-11-26 00:01:51 -0800

    added table alias to query

--- appService.js
+++ appService.js
@@ -214,7 +214,7 @@ async function selectStops(selectedStopName) {
 async function joinTripsplan2People(name, transitCard) {
 //    const sqlQuery = `SELECT tp.startTime, tp.arrivalLocation, tp.departureLocation FROM TripsPlan2 tp, People
 //    WHERE p.peopleName=:name AND p.transitCardNumber=:transitCard`
-    const sqlQuery = `SELECT tp.startTime, tp.arrivalLocation, tp.departureLocation FROM TripsPlan2 tp, People
+    const sqlQuery = `SELECT tp.startTime, tp.arrivalLocation, tp.departureLocation FROM TripsPlan2 tp, People p
              WHERE p.customerID = tp.customerID AND p.peopleName=${name} AND p.transitCardNumber=${transitCard}`
     return await withOracleDB(async (connection) => {
         const result = await connection.execute(sqlQuery);
